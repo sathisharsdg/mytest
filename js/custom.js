74f0bd708b175a5b30bb0792ef34d1dca218892f
Sathish <sathish.333ddd@gmail.com>
2020-05-06 11:16:16 +0530

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,15 @@
 //Custom Js goes here
 
-   $(document).ready(function(){
-	   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() - 70;
-			 if ($(window).scrollTop() > navHeight) {
-				 $('#navbarText').addClass('fixed-class');
-			 }
-			 else {
-				 $('#navbarText').removeClass('fixed-class');
-			 }
-		});
+$(document).ready(function(){
+	// Add minus icon for collapse element which is open by default
+	$(".collapse.show").each(function(){
+		$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
 	});
+	
+	// Toggle plus minus icon on show hide of collapse element
+	$(".collapse").on('show.bs.collapse', function(){
+		$(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+	}).on('hide.bs.collapse', function(){
+		$(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+	});
+});
